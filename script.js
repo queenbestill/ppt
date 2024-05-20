cf4d01d1ad75ab318395fd128f7e7165d608c5d3
queenbestill <inglescanarias@gmail.com>
2024-05-20 16:39:06 +0100

    primer día

--- script.js
+++ script.js
@@ -8,28 +8,25 @@ rock.addEventListener('click',function () {
     selectUser('rock')
 })
 
-
-
 paper.addEventListener('click',function () {
-    alert('paper')
+    selectUser('papel')
 })
 
-
 scissors.addEventListener('click',function () {
-    alert('scissors')
+    selectUser('tijeras')
 })
 
 
 function selectUser(player){
 
-    console.log(player)
-
-    if(player === "piedra" && pc === "papel"){
+    let pcselect = generarAleatorioEntre0y2()
+    console.log(pcselect)
+    if(player === "piedra" && pcselect === "papel"){
         return "Gana ordenador"
     } 
-    else if(player === "papel" && pc === "tijera"){
+    else if(player === "papel" && pcselect === "tijera"){
         return "Gana ordenador"
-    } else if(player === "tijera" && pc ==="piedra"){
+    } else if(player === "tijera" && pcselect ==="piedra"){
         return "Gana ordenador"
     }
     else {
@@ -37,19 +34,18 @@ function selectUser(player){
     }
 }
 
-const selections = document.querySelectorAll('data-selection')
+
 
 
 
 function generarAleatorioEntre0y2() {
   let a = Math.floor(Math.random() * 3);
-  if (a ===0) {
-   return console.log('piedra')
-  } 
-  else if (a===1) {
-   return console.log('papel')
-} 
-    else {
-   return console.log('tijera')
-} 
- }
+  if (a === 0) {
+    return 'piedra';
+  } else if (a === 1) {
+    return 'papel';
+  } else {
+    return 'tijera';
+  }
+}
+
