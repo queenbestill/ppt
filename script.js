a4e156b00214bed444c0789cee3a24744c08d75a
queenbestill <inglescanarias@gmail.com>
2024-05-20 15:00:32 +0100

    Co-authored-by: Fragomo <Fragomo@users.noreply.github.com> Co-authored-by: noemimd92 <noemimd92@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,16 @@
+const rock = document.querySelector('#piedra-img')
+const paper = document.querySelector('#papel-img')
+const scissors = document.querySelector('#tijera-img')
+
+rock.addEventListener('click',function () {
+    alert('rock')
+})
+
+paper.addEventListener('click',function () {
+    alert('paper')
+})
+
+scissors.addEventListener('click',function () {
+    alert('scissors')
+})
+
