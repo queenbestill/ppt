a099f7c4343f4fa6646b187a75dda226bdb2e690
queenbestill <inglescanarias@gmail.com>
2024-05-22 16:53:37 +0100

    agregar musica

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-const rock = document.querySelector('#piedra-img');
-const paper = document.querySelector('#papel-img');
-const scissors = document.querySelector('#tijera-img');
-const jugadorChoice = document.getElementById('jugador-choice');
-const ordenadorChoice = document.getElementById('ordenador-choice');
-const jugadorChoiceImg = document.getElementById('jugador-choice-img');
-const ordenadorChoiceImg = document.getElementById('ordenador-choice-img');
-const contarTirada = document.getElementById("tirada")
-const tiradasPendientes = document.getElementById("restantes")
-const dialog=document.getElementById('cuadro')
-let resetButton
-console.log(dialog)
+const rock = document.querySelector("#piedra-img");
+const paper = document.querySelector("#papel-img");
+const scissors = document.querySelector("#tijera-img");
+const jugadorChoice = document.getElementById("jugador-choice");
+const ordenadorChoice = document.getElementById("ordenador-choice");
+const jugadorChoiceImg = document.getElementById("jugador-choice-img");
+const ordenadorChoiceImg = document.getElementById("ordenador-choice-img");
+const contarTirada = document.getElementById("tirada");
+const tiradasPendientes = document.getElementById("restantes");
+const dialog = document.getElementById("cuadro");
+let resetButton;
+console.log(dialog);
 
 let jugadorScore = 0;
 let ordenadorScore = 0;
 let turno = 0;
-let inicial = document.getElementById("#inicial")
-dialog.style.display = 'none'
+let inicial = document.getElementById("#inicial");
+dialog.style.display = "none";
 
-rock.addEventListener('click', function () {
-  jugar('piedra');
+rock.addEventListener("click", function () {
+  jugar("piedra");
 });
-paper.addEventListener('click', function () {
-  jugar('papel');
+paper.addEventListener("click", function () {
+  jugar("papel");
 });
-scissors.addEventListener('click', function () {
-  jugar('tijera');
+scissors.addEventListener("click", function () {
+  jugar("tijera");
 });
 
 function jugar(selecUsuario) {
   if (turno === 3) {
-    return }
-  // actualizar la jugada para que sea segunda 
+    return;
+  }
+  // actualizar la jugada para que sea segunda
   turno++;
   contarTirada.innerText = turno;
   tiradasPendientes.innerText = 3 - turno;
-
 
   let selecOrdenador = generarAleatorioEntre0y2();
   let resultado = determinarGanador(selecUsuario, selecOrdenador);
@@ -45,9 +45,9 @@ function jugar(selecUsuario) {
   mostrarImagenSeleccion(jugadorChoiceImg, selecUsuario);
   mostrarImagenSeleccion(ordenadorChoiceImg, selecOrdenador);
 
-  if (resultado === 'Gana jugador') {
+  if (resultado === "Gana jugador") {
     jugadorScore++;
-  } else if (resultado === 'Gana ordenador') {
+  } else if (resultado === "Gana ordenador") {
     ordenadorScore++;
   }
 
@@ -56,7 +56,7 @@ function jugar(selecUsuario) {
   console.log(resultado);
 
   if (turno === 3) {
-    dialog.style.display = 'block';
+    dialog.style.display = "block";
     mostrarDialogo(resultado);
   }
 }
@@ -69,7 +69,6 @@ function determinarGanador(player, pcselect) {
     (player === "papel" && pcselect === "piedra") ||
     (player === "tijera" && pcselect === "papel")
   ) {
-
     return "Gana jugador";
   } else {
     return "Gana ordenador";
@@ -79,66 +78,64 @@ function determinarGanador(player, pcselect) {
 function generarAleatorioEntre0y2() {
   let a = Math.floor(Math.random() * 3);
   if (a === 0) {
-    return 'piedra';
+    return "piedra";
   } else if (a === 1) {
-    return 'papel';
+    return "papel";
   } else {
-    return 'tijera';
+    return "tijera";
   }
 }
 
 function actualizarPuntuacion() {
-  document.getElementById('jugador-score').innerText = jugadorScore;
-  document.getElementById('ordenador-score').innerText = ordenadorScore;
+  document.getElementById("jugador-score").innerText = jugadorScore;
+  document.getElementById("ordenador-score").innerText = ordenadorScore;
 }
 
 function mostrarImagenSeleccion(element, seleccion) {
-  element.style.display = 'block';
-  let imagen = document.createElement('img');
-  if (seleccion === 'piedra') {
-    imagen.src = 'img/piedradigi.png';
-    imagen.style.height = '15vh';
-  } else if (seleccion === 'papel') {
-    imagen.src = 'img/papeldigi.png';
-    imagen.style.height = '15vh';
-  } else if (seleccion === 'tijera') {
-    imagen.src = 'img/tijeradigi.png';
-    imagen.style.height = '15vh';
+  element.style.display = "block";
+  let imagen = document.createElement("img");
+  if (seleccion === "piedra") {
+    imagen.src = "img/piedradigi.png";
+    imagen.style.height = "15vh";
+  } else if (seleccion === "papel") {
+    imagen.src = "img/papeldigi.png";
+    imagen.style.height = "15vh";
+  } else if (seleccion === "tijera") {
+    imagen.src = "img/tijeradigi.png";
+    imagen.style.height = "15vh";
   }
-  element.innerHTML = '';
+  element.innerHTML = "";
   element.appendChild(imagen);
 }
 
 function mostrarDialogo(resultado) {
-  resetButton = document.createElement('button')
+  resetButton = document.createElement("button");
 
-  resetButton.setAttribute('id','reset-button')
-  resetButton.innerText= 'Reiniciar'
-  dialog.appendChild(resetButton)
-  
-  const message = document.getElementById('game-over-dialog');
+  resetButton.setAttribute("id", "reset-button");
+  resetButton.innerText = "Reiniciar";
+  dialog.appendChild(resetButton);
+
+  const message = document.getElementById("game-over-dialog");
   if (jugadorScore > ordenadorScore) {
     message.innerText = "¡Ganaste!";
     message.style.color = "green";
   } else {
     message.innerText = "¡Perdiste!";
-    message.style.color="red";
+    message.style.color = "red";
   }
 
-  
   resetButton.onclick = function () {
-
     jugadorScore = 0;
     ordenadorScore = 0;
     turno = 0;
     actualizarPuntuacion();
-    dialog.style.display = 'none';
-    dialog.removeChild(resetButton)
-    jugadorChoice.innerText = '-';
-    ordenadorChoice.innerText = '-';
-    jugadorChoiceImg.style.display = 'none';
-    ordenadorChoiceImg.style.display = 'none';
-  }
+    dialog.style.display = "none";
+    dialog.removeChild(resetButton);
+    jugadorChoice.innerText = "-";
+    ordenadorChoice.innerText = "-";
+    jugadorChoiceImg.style.display = "none";
+    ordenadorChoiceImg.style.display = "none";
+  };
 }
 
 const inicio = document.getElementById("inicial");
@@ -148,13 +145,19 @@ newInicio.innerHTML = "<h1>Bienvenidos a Piedra, papel y tijeras</h1>";
 inicio.appendChild(newInicio);
 
 const botonInicio = document.getElementById("start");
-botonInicio.addEventListener("click", function() {
+botonInicio.addEventListener("click", function () {
   console.log(inicio);
   inicio.style.display = "none";
   document.getElementById("juego").style.display = "block"; // Mostrar el juego
 });
 
 // Ocultar el juego al cargar la página
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("juego").style.display = "none";
 });
+
+const audio = document.createElement("audio");
+audio.preload = "auto";
+audio.src = "lady-of-the-80x27s-128379.mp3";
+audio.play();
+document.body.appendChild(audio);
